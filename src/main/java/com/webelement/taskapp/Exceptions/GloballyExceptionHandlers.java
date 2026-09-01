61245083b073ccfbb892f962b4f5d4dafcf8b424
Sunil Saklani <sunil.saklani@webelementinc.com>
2026-09-01 18:19:49 +0530

    changed

--- src/main/java/com/webelement/taskapp/Exceptions/GloballyExceptionHandlers.java
+++ src/main/java/com/webelement/taskapp/Exceptions/GloballyExceptionHandlers.java
@@ -7,22 +7,25 @@ import com.webelement.taskapp.common.ResponseApi;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
 @RestControllerAdvice
 public class GloballyExceptionHandlers {
-	  @ExceptionHandler(FileValidationException.class)
-	    public ResponseEntity<ResponseApi<String>> handleFileValidationException(
-	            FileValidationException ex) {
+	@ExceptionHandler(ClientValidationException.class)
+    public ResponseEntity<ResponseApi<String>> handleClientValidationException(ClientValidationException ex) {
 
-	        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-	                .body(new ResponseApi<>(
-	                        false,
-	                        ex.getMessage(),
-	                        null));
-	    }
-		@ExceptionHandler(Exception.class)
-		public ResponseEntity<ResponseApi<String>> handleException(Exception ex) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(new ResponseApi<>(false, "Something went wrong", null));
-		}
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseApi<>(false, ex.getMessage(), null));
+    }
+	
+	@ExceptionHandler(FileValidationException.class)
+	public ResponseEntity<ResponseApi<String>> handleFileValidationException(FileValidationException ex) {
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseApi<>(false, ex.getMessage(), null));
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ResponseApi<String>> handleException(Exception ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(new ResponseApi<>(false, "Something went wrong", null));
+	}
 }
